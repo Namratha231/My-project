3875e76b37a6eabee330c607888004a2f519b293
Namratha231 <85865557+Namratha231@users.noreply.github.com>
2023-04-01 15:20:34 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,25 @@
+var arr=[9,9,0,8,0,0,2,6,8,5]
+var sum =0
+for(let i=0;i<arr.length;i++)
+{
+  sum +=arr[i];
+}
+for(var i=1;i<=sum;i++)
+{
+  if (i%3==0)
+  {
+    process.stdout.write('Fizz'+" ");
+  }
+  else if(i%4==0)
+  {
+    process.stdout.write('Buzz'+" ");
+  }
+  else if(i%12==0)
+  {
+    process.stdout.write('FizzBuzz'+" ");
+  }
+  else
+  {
+    process.stdout.write(i+" ");
+  }
+}
